012b534a3ef2dab72be04dff188fdb9280b37310
jesusz-yeboyebo <jesus.yeboyebo@gmail.com>
2025-03-20 10:57:12 +0100

    Control de caracteres GLS

--- ctrl_api_gls_glsenvio__get_envio.py
+++ ctrl_api_gls_glsenvio__get_envio.py
@@ -20,6 +20,69 @@ class gls_postenviogls(interna_get):
 
     @staticmethod
     def start(self, data):
+        print(data)
+
+        oCaracteres = {}
+        oCaracteres["Á"] = "A"
+        oCaracteres["É"] = "E"
+        oCaracteres["Í"] = "I"
+        oCaracteres["Ó"] = "O"
+        oCaracteres["Ú"] = "U"
+        oCaracteres["á"] = "a"
+        oCaracteres["é"] = "e"
+        oCaracteres["í"] = "i"
+        oCaracteres["ó"] = "o"
+        oCaracteres["ú"] = "u"
+        oCaracteres["ñ"] = "n"
+        oCaracteres["Ñ"] = "N"
+        oCaracteres["À"] = "A"
+        oCaracteres["È"] = "E"
+        oCaracteres["Ì"] = "I"
+        oCaracteres["Ò"] = "O"
+        oCaracteres["Ù"] = "U"
+        oCaracteres["à"] = "a"
+        oCaracteres["è"] = "e"
+        oCaracteres["ì"] = "i"
+        oCaracteres["ò"] = "o"
+        oCaracteres["ù"] = "u"
+        oCaracteres["Â"] = "A"
+        oCaracteres["Ê"] = "E"
+        oCaracteres["Î"] = "I"
+        oCaracteres["Ô"] = "O"
+        oCaracteres["Û"] = "U"
+        oCaracteres["â"] = "a"
+        oCaracteres["ê"] = "e"
+        oCaracteres["î"] = "i"
+        oCaracteres["ô"] = "o"
+        oCaracteres["û"] = "u"
+        oCaracteres["Ä"] = "A"
+        oCaracteres["Ë"] = "E"
+        oCaracteres["Ï"] = "I"
+        oCaracteres["Ö"] = "O"
+        oCaracteres["Ü"] = "U"
+        oCaracteres["ä"] = "a"
+        oCaracteres["ë"] = "e"
+        oCaracteres["ï"] = "i"
+        oCaracteres["ö"] = "o"
+        oCaracteres["ü"] = "u"
+        oCaracteres["ü"] = "u"
+        oCaracteres["ç"] = "c"
+        oCaracteres["Ç"] = "C"
+        oCaracteres[";"] = ""
+        oCaracteres["'"] = ""
+        oCaracteres[","] = ""
+        oCaracteres["\""] = ""
+        oCaracteres["\r\n"] = " "
+        oCaracteres["\r"] = " "
+        oCaracteres["\n"] = " "
+        oCaracteres["\t"] = " "
+
+        for datos in data:
+            for c in oCaracteres:
+                cadena = str(data[datos])
+                cadena = cadena.replace(c, oCaracteres[c])
+                data[datos] = cadena
+
         if "DatosDestinatario_Email" not in data:
             data["DatosDestinatario_Email"] = ""
         
@@ -37,13 +100,13 @@ class gls_postenviogls(interna_get):
                 observaciones = "<Observaciones>" + data["obsParcelShop"] + "</Observaciones><Codigo>" + data["codParcelShop"] + "</Codigo>"
 
         payload = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\"><soap12:Body><GrabaServicios xmlns=\"http://www.asmred.com/\"><docIn><Servicios uidcliente=\"" + data["IdCliente"] + "\"><Envio codbarras=\"\"><Fecha>" + data["DatosServicio_Fecha"] + "</Fecha><Portes>P</Portes><Servicio>" + data["Servicio_GLS"] + "</Servicio><Horario>" + data["Horario_GLS"] + "</Horario><Bultos>" + data["Bultos"] + "</Bultos><Peso>000000.620</Peso><Volumen>000000.011</Volumen><DNINomb>0</DNINomb><FechaPrevistaEntrega/><Retorno>0</Retorno><Pod>N</Pod><PODObligatorio>N</PODObligatorio><Aduanas><Incoterm>" + incotermGLS + "</Incoterm></Aduanas><Remite><Nombre>" + data["DatosRecogida_Nombre"] + "</Nombre><Direccion>" + data["DatosRecogida_Direccion"] + "</Direccion><Poblacion>" + data["DatosRecogida_Poblacion"] + "</Poblacion><Provincia>" + data["DatosRecogida_Provincia"] + "</Provincia><Pais>" + data["DatosRecogida_Pais"] + "</Pais><CP>" + data["DatosRecogida_CodPostal"] + "</CP><NIF>" + data["DatosRecogida_CifNif"] + "</NIF><Telefono>" + data["DatosRecogida_Telefono"] + "</Telefono><Movil>" + data["DatosRecogida_Telefono"] + "</Movil><Email>" + data["DatosRecogida_Email"] + "</Email><Observaciones>" + data["DatosCodPedido"] + " - " + data["DatosRecogida_Contacto"] + "</Observaciones></Remite><Destinatario><Nombre>" + data["DatosDestinatario_Nombre"] + "</Nombre><Direccion>" + data["DatosDestinatario_Direccion"] + "</Direccion><Poblacion>" + data["DatosDestinatario_Poblacion"] + "</Poblacion><Provincia>" + data["DatosDestinatario_Provincia"] + "</Provincia><Pais>" + data["DatosDestinatario_Pais"] + "</Pais><CP>" + data["DatosDestinatario_CodPostal"] + "</CP><Telefono>" + data["DatosDestinatario_Telefono"] + "</Telefono><Movil>" + data["DatosDestinatario_Telefono"] + "</Movil><Email>" + data["DatosDestinatario_Email"] + "</Email><ATT>" + data["DatosCodPedido"] + " - " +  data["DatosDestinatario_Nombre"] + "</ATT><NIF/>" + observaciones + "</Destinatario><Referencias><Referencia tipo=\"C\">" + data["DatosCodigoOperacion"] + "</Referencia></Referencias><DevuelveAdicionales><Etiqueta tipo=\"PDF\"/></DevuelveAdicionales></Envio></Servicios></docIn></GrabaServicios></soap12:Body></soap12:Envelope>"
+        #print(str(payload))
 
         headers = {
           'Content-Type': 'text/xml; charset=utf-8'
         }
 
         print(str(payload.encode("utf8")))
-
         wsdl = 'https://wsclientes.asmred.com/b2b.asmx?op=GrabaServicios'
 
         r = requests.request("POST", wsdl, headers=headers, data=payload)
